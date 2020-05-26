94ccf1c853865ac2ab810d5c02014969ff6938f6
Camila Cunha <millagerarde@gmail.com>
2020-05-26 17:52:47 -0300

    Refatoração data

--- src/data.js
+++ src/data.js
@@ -1,4 +1,4 @@
-let dictionary = {
+const translatedList = {
   "Grass": "Planta",
   "Poison": "Venenoso",
   "Fire": "Fogo",
@@ -20,100 +20,96 @@ let dictionary = {
 }
 
 function translate(element) {
-  return dictionary[element]
+  return translatedList[element];
 }
 
-
 /**
- * Função para selecionar informações doa array pokemon que serão mostradas.
- * @param {Array.<Object>} data Array contendo lista de objetos(151 pokemons).
- * @returns {Array.<Object>} Retorna uma variavel newListCard, contendo uma lista com informações selecionadas.
+ * Função para selecionar informações do array que serão mostradas.
+ * @param {Array.<Object>} data Array contendo lista de objetos(151 pokémons).
+ * @returns {Array.<Object>} Retorna uma variável newListCard, contendo uma lista com informações selecionadas,
  * no objeto anterior.
  */
 export function selectInfosToShow(data) {
-  if (!Array.isArray(data) || data.length === 0) {
-    throw new TypeError("parâmetro invalido")
-  }
-  let verifiedCandy
-  let listPokemon = data;
-  let newListCard = [];
-  for (let pokemon of listPokemon) {
-    verifiedCandy = pokemon.candy_count
-    if (verifiedCandy === undefined) {
-      verifiedCandy = "Não possui evolução"
+  let listCard = [];
+  if (!Array.isArray(data) || !data.length) {
+    throw new TypeError("parâmetro inválido");
+  } else {
+    const listPokemon = data;
+    for (let pokemon of listPokemon) {
+      let typesTranslate = pokemon.type.map(translate);
+      let weaknessesTranslate = pokemon.weaknesses.map(translate);
+
+      let verifiedCandy = pokemon.candy_count;
+      if (verifiedCandy === undefined) {
+        verifiedCandy = "Não possui evolução";
+      }
+
+      let infosCard = {
+        number: pokemon.num,
+        name: pokemon.name,
+        image: pokemon.img,
+        types: typesTranslate.join(", "),
+        weaknesses: weaknessesTranslate.join(", "),
+        probability: pokemon.spawn_chance,
+        height: pokemon.height,
+        weight: pokemon.weight,
+        candy: "Doce de " + pokemon.candy.replace("Candy", ""),
+        candy_count: verifiedCandy,
+        egg: pokemon.egg.replace("Not in Eggs", "Não Nasce em Ovos"),
+      }
+      listCard.push(infosCard);
     }
-    let tipos = pokemon.type.map(translate)
-    let fraquezas = pokemon.weaknesses.map(translate)
-    let infosCard = {
-      number: pokemon.num,
-      name: pokemon.name,
-      image: pokemon.img,
-      types: tipos.join(", "),
-      weaknesses: fraquezas.join(", "),
-      probability: pokemon.spawn_chance,
-      height: pokemon.height,
-      weight: pokemon.weight,
-      candy: "Doce de " + pokemon.candy.replace("Candy", ""),
-      candy_count: verifiedCandy,
-      egg: pokemon.egg.replace("Not in Eggs", "Não Nasce em Ovos"),
-    }
-    newListCard.push(infosCard)
   }
-  return newListCard
+  return listCard;
 }
 
 /**
  * Função que compara dois elementos para definir a ordem de posicionamento do menor para o maior.
- * @param {Object} objeto1 Primeiro objeto a ser comparado (um intem da lista (um pokemon)).
- * @param {Object} objeto2 Segundo  objeto a ser comparado (um intem da lista (um pokemon)).
- * @param {String} option  Uma propriedade do objeto reperesentada por uma string. Ex: no select tipo "type-Grass".
- * @returns Retorna a posição do elemento: -1 = para atŕas; 1 = para frente e 0 = mantém a posição.
+ * @param {Object} objeto1 Primeiro objeto a ser comparado (item da lista (um Pokémon)).
+ * @param {Object} objeto2 Segundo  objeto a ser comparado (item da lista (um Pokémon)).
+ * @param {String} option  Uma propriedade do objeto representada por uma string. Ex: no select tipo "type-Grass".
+ * @returns Retorna a posição do elemento: -1 = para atrás; 1 = para frente e 0 = mantém a posição.
  */
-
 function sortCrescent(objeto1, objeto2, option) {
   if (objeto1[option] < objeto2[option]) {
-    return -1
+    return -1;
+  } else if (objeto1[option] > objeto2[option]) {
+    return 1;
+  } else {
+    return 0;
   }
-  if (objeto1[option] > objeto2[option]) {
-    return 1
-  }
-  return 0
 }
 
 /**
- * Função recebe lista de pokemons e ordena de acordo com o parametro.
- * @param {Array.<Object>} data Array contendo lista de objetos(151 pokemons).
- * @param {string} option Uma propriedade do objeto reperesentada por uma string. Ex: no select ordenar "name-increasing".
- * @param {string} order  Uma string ("increasing"/"decreasing")que indica se a ordenação srá crescente ou decrescente.
+ * Função recebe lista de Pokémons e ordena de acordo com o parâmetro.
+ * @param {Array.<Object>} data Array contendo lista de objetos(151 Pokémons).
+ * @param {string} option Uma propriedade do objeto representada por uma string. Ex: no select ordenar "name-increasing".
+ * @param {string} order  Uma string ("increasing"/"decreasing")que indica se a ordenação será crescente ou decrescente.
  * @returns uma lista ordenada.
  */
 export function ordenation(data, option, order) {
-  if (
-    !Array.isArray(data) || data.length === 0,
-    typeof option != "string" || option.length === 0,
-    typeof order != "string" || order.length === 0
-  ) {
-    throw new TypeError("parâmetro invalido")
-  }
-
-  let listPokemon = data
   let sortedList = []
-  if (order === "increasing") {
-    sortedList = listPokemon.sort(function (a, b) {
-      return sortCrescent(a, b, option)
-    })
+  if (
+    !Array.isArray(data) || !data.length,
+    typeof option !== "string" || !option.length,
+    typeof order !== "string" || !order.length
+  ) {
+    throw new TypeError("parâmetro inválido");
+  } else {
+    const listPokemon = data;
+    if (order === "increasing") {
+      sortedList = listPokemon.sort((a, b) => sortCrescent(a, b, option));
+    }
+    if (order === "decreasing") {
+      sortedList = listPokemon.sort((a, b) => sortCrescent(a, b, option)).reverse();
+    }
+    return sortedList;
   }
-  if (order === "decreasing") {
-    sortedList = listPokemon.sort(function (a, b) {
-      return sortCrescent(a, b, option)
-    }).reverse()
-  }
-  return sortedList
 }
 
 /**
  * Função que compara searchedValue com a propriedade buscada.
- * @param {Object} objeto Intém(pokemon)da lista de pokemons que tera suas propriedades verificadas.
+ * @param {Object} objeto Item(Pokémon)da lista de Pokémons que terá suas propriedades verificadas.
  * @param {string} option Uma propriedade(ex:name)do objeto reperesentada por uma string.
  * @param {string} searchedValue Uma string representando o valor da buscado. 
  * @returns Comparação entre o objeto e o searchedValue convertendo para LowerCase.
@@ -122,17 +118,21 @@ function compareSearchedValue(objeto, option, searchedValue) {
   if (Array.isArray(objeto[option])) {
     for (let element of objeto[option]) {
       if (element === searchedValue) {
-        return true
+        return true;
       }
     }
   } else {
-    return objeto[option].toLowerCase() === searchedValue.toLowerCase()
+    return objeto[option].toLowerCase() === searchedValue.toLowerCase();
   }
 }
 
+
+//parei aqui
+
+
 /**
  * Função aplica a seleção no array de acordo com o SearchedValue.
- * @param {Array.<Object>} data Array contendo lista de objetos(151 pokemons).
+ * @param {Array.<Object>} data Array contendo lista de objetos(151 Pokémons).
  * @param {string} option Uma propriedade(ex:num)do objeto reperesentada por uma string.
  * @param {string} searchedValue uma string representando qualquer valor da buscado no array (o valor da propriedade).
  * @returns Uma lista contendo os objetos filtrados.
@@ -155,9 +155,9 @@ export function filterInfons(data, option, searchedValue) {
 
 /**
  * Função realiza o calculo do futuro CP após evolução.
- * @param {Array.<Object>} data Array contendo lista de objetos(151 pokemons).
- * @param {number} currentCp Cp atual do pokemon representado por um numero.
- * @param {string} namePokemon Nome do pokemon buscado que terá o futuro CP calculado.
+ * @param {Array.<Object>} data Array contendo lista de objetos(151 Pokémons).
+ * @param {number} currentCp Cp atual do Pokémon representado por um numero.
+ * @param {string} namePokemon Nome do Pokémon buscado que terá o futuro CP calculado.
  * @returns Um objeto contendo valor max/min e medio do futuro cp.
  */
 export function computeCp(data, currentCp, namePokemon) {
@@ -194,10 +194,10 @@ export function computeCp(data, currentCp, namePokemon) {
 
 /**
  * Função que acessa o array e seleciona os objetos de interesse 
- * (ex: a proxima evolução do pokemon filtrado).
- * @param {Array.<Object>} data Array contendo lista de objetos(151 pokemons).
- * @param {Object} namePokemon Intém(pokemon)da lista de pokemons, 
- * que representa a proxima evolução do pokemon filtrado.  
+ * (ex: a proxima evolução do Pokémon filtrado).
+ * @param {Array.<Object>} data Array contendo lista de objetos(151 Pokémons).
+ * @param {Object} namePokemon Intém(Pokémon)da lista de Pokémons, 
+ * que representa a proxima evolução do Pokémon filtrado.  
  * @returns Uma lista com um objeto.
  */
 export function getNextEvolution(data, namePokemon) {
@@ -218,8 +218,8 @@ export function getNextEvolution(data, namePokemon) {
 }
 
 /**
- * Função realiza calculo estatístico da porcentagem de pokemons por tipo.
- * @param {Array.<Object>} data Array contendo lista de objetos(151 pokemons).
+ * Função realiza calculo estatístico da porcentagem de Pokémons por tipo.
+ * @param {Array.<Object>} data Array contendo lista de objetos(151 Pokémons).
  * @param {string} option  Uma propriedade(ex:tipo)do objeto reperesentada por uma string.
  * @param {Array.<String>} arrayTypes Array contendo valores que serão filtrados para calculo.
  */
